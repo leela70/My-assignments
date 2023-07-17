f07b67d11c09525fdc448329de50c7b6493f1aaf
leela70 <139783211+leela70@users.noreply.github.com>
2023-07-17 21:38:50 +0530

    Add files via upload
    Day1

---
+++ day1/Mobile.java
@@ -0,0 +1,33 @@
+package Week1.day1;
+
+public class Mobile {
+      private static final Mobile MyMobile = null;
+	public void Talking()
+      {
+    	  System.out.println("Talking");
+      }
+      public void chatting()
+      {
+    	  System.out.println("chatting");
+      }
+      public void reading()
+      {
+    	  System.out.println("reading");
+      }
+      public void watching()
+      {
+    	  System.out.println("watching");
+      }
+      public static void main(String[]args)
+      {
+            Mobile MyMobile=new Mobile();
+            MyMobile.Talking();
+            MyMobile.chatting();
+            MyMobile.reading();
+            MyMobile.watching();
+
+      }
+}
+      
+
+
